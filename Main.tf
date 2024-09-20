ff816d01593e666744dc191a4fc0dd9d37fcb005
preshit garg <p.garg125@gmail.com>
2024-09-20 15:21:54 +0530

    this is my seven commit

--- Main.tf
+++ Main.tf
@@ -1,7 +1,7 @@
 #Create VPC - 10.200.0.0/16
-resource "aws_vpc" "Git-vpc3" {
+resource "aws_vpc" "Git-vpc4" {
   cidr_block = "10.200.0.0/16"
   tags = {
-    "Name" = "Git-vpc3"
+    "Name" = "Git-vpc4"
   }
 }
