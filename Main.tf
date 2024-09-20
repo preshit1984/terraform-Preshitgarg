f1b66027305bdde5fa7e7d6db9e11e9433bdda11
preshit garg <p.garg125@gmail.com>
2024-09-20 15:37:22 +0530

    this is my ten commit

--- Main.tf
+++ Main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "Git-vpc5" {
 }
 #Create Subnet - 10.0.1.0/24
 resource "aws_subnet" "pre-websunet1" {
-  vpc_id                  = aws_vpc.pre-vpc.id
+  vpc_id                  = aws_vpc.Git-vpc5.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
@@ -17,7 +17,7 @@ resource "aws_subnet" "pre-websunet1" {
 }
 #Create Subnet - 10.0.2.0/24
 resource "aws_subnet" "pre-websunet2" {
-  vpc_id                  = aws_vpc.pre-vpc.id
+  vpc_id                  = aws_vpc.Git-vpc5.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
